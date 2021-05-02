f1c7c40c6978863eae93e63779b3e9cdca28d14a
jhoncemplok <jhoncemplok@gmail.com>
2021-05-02 07:43:02 +0000

    tambah baru

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
+/**ini file test  **/
 public class HelloWorld {
     public static void main (String [] args) {
      System.out.println("Hello World again");
      System.out.println("Hai!");
     }
 }
+
